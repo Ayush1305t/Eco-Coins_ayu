e3eb263ee179794c287375228c92f55e60bff2c7
Akshat Sarawat <akshatsaraswat29@gmail.com>
2026-04-02 00:07:02 +0530

    feat: Add Dashboard component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import CleaningValidationPage from "./pages/CleaningValidationPage";
 import CleaningFlow from "./pages/CleaningFlow";
 import Home from "./pages/Home";
+import Dashboard from "./components/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/clean" element={<CleaningFlow />} />
         <Route path="/validation" element={<CleaningValidationPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
